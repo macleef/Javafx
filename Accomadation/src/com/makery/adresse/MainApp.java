2989a60a65da8910d9a4d4a48bcbe0802b86626c
macleef <macleef@gmail.com>
2015-07-13 20:44:26 +0200

    initilisation du menu File

--- Accomadation/src/com/makery/adresse/MainApp.java
+++ Accomadation/src/com/makery/adresse/MainApp.java
@@ -1,8 +1,15 @@
 package com.makery.adresse;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.prefs.Preferences;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 
 import com.makery.adresse.model.Person;
+import com.makery.adresse.model.PersonListWrapper;
 import com.makery.adresse.view.PersonEditDialogController;
 import com.makery.adresse.view.PersonOverviewControll;
 
@@ -11,6 +18,8 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -139,5 +148,94 @@ public class MainApp extends Application {
     public ObservableList<Person> getPersonData() {
         return personData;
     }
+    
+    public File getPersonFilePath() {
+    	Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+    	String filePath = prefs.get("filePath", null);
+    	if (filePath != null) {
+    		return new File(filePath);
+    	} else {
+    		return null;
+    	}
+    	
+    }
+    
+    public void setPersonFilePath(File file) {
+        Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+        if (file != null) {
+            prefs.put("filePath", file.getPath());
+
+            // Update the stage title.
+            primaryStage.setTitle("AddressApp - " + file.getName());
+        } else {
+            prefs.remove("filePath");
+
+            // Update the stage title.
+            primaryStage.setTitle("AddressApp");
+        }
+    }
+    
+    /**
+     * Loads person data from the specified file. The current person data will
+     * be replaced.
+     * 
+     * @param file
+     */
+    public void loadPersonDataFromFile(File file) {
+        try {
+            JAXBContext context = JAXBContext
+                    .newInstance(PersonListWrapper.class);
+            Unmarshaller um = context.createUnmarshaller();
+
+            // Reading XML from the file and unmarshalling.
+            PersonListWrapper wrapper = (PersonListWrapper) um.unmarshal(file);
+
+            personData.clear();
+            personData.addAll(wrapper.getPersons());
+
+            // Save the file path to the registry.
+            setPersonFilePath(file);
+
+        } catch (Exception e) { // catches ANY exception
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Could not load data");
+            alert.setContentText("Could not load data from file:\n" + file.getPath());
+
+            alert.showAndWait();
+        }
+    }
+
+    /**
+     * Saves the current person data to the specified file.
+     * 
+     * @param file
+     */
+    public void savePersonDataToFile(File file) {
+        try {
+            JAXBContext context = JAXBContext
+                    .newInstance(PersonListWrapper.class);
+            Marshaller m = context.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            // Wrapping our person data.
+            PersonListWrapper wrapper = new PersonListWrapper();
+            wrapper.setPersons(personData);
+
+            // Marshalling and saving XML to the file.
+            m.marshal(wrapper, file);
+
+            // Save the file path to the registry.
+            setPersonFilePath(file);
+        } catch (Exception e) { // catches ANY exception
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Could not save data");
+            alert.setContentText("Could not save data to file:\n" + file.getPath());
+
+            alert.showAndWait();
+        }
+    }
+    
 }
 
---
+++ Accomadation/src/com/makery/adresse/model/PersonListWrapper.java
@@ -0,0 +1,26 @@
+package com.makery.adresse.model;
+
+
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@XmlRootElement(name = "persons")
+public class PersonListWrapper {
+	
+	private List<Person> persons;
+	
+	@XmlElement(name = "person")
+	public List<Person> getPersons(){
+		return persons;
+	}
+	
+	public void setPersons(List<Person> persons) {
+		this.persons = persons;
+	}
+	
+
+}
